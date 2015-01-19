7b7f84871636841982a4b4c0b01ee6c63eb9ff19
DucAnhPhi <ducphi1@yahoo.de>
2015-01-19 17:31:25 +0100

    Aktueller Stand
    man kann jetzt personen hinzufügen Gegenstände hinzu fügen und sie
    einsammeln

---
+++ __SHELL30.java
@@ -0,0 +1,10 @@
+
+public class __SHELL30 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\Keoma\\Documents\\ZOOL");
+final Game game1 = (Game)__bluej_runtime_scope.get("game1");
+
+
+game1.play();
+
+}}
